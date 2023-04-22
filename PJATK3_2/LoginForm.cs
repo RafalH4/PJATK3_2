fbf2f62ed41e4fb2c1b41217b0056201c4de220a
Rafał Huk <r.huk@outlook.com>
2023-04-22 17:56:08 +0200

    filmik 1

--- PJATK3_2/LoginForm.cs
+++ PJATK3_2/LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,28 @@ namespace PJATK3_2
                 MessageBox.Show("Wprowadź login i hasło");
                 return;
             }
+            //1. Konfiguracja połączenia i zapytania
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-LQFAVEE;Initial Catalog=API_L4;Integrated Security=True");
+            SqlCommand com = new SqlCommand();
+            com.Connection = con;
+            com.CommandText = "Select * from uzytkownik";
+
+            //2. Otwarcie połączenia
+            con.Open();
+
+            //3. Wykonanie zapytania
+            SqlDataReader dr = com.ExecuteReader();
+
+            //4. Odczyt rezultatu
+            while (dr.Read())
+            {
+                string login = dr["Login"].ToString();
+                MessageBox.Show(login);
+            }
+
+            //5. Zamknięcie połączenia
+            con.Dispose();
+
             var sql = $"select * from osoba where login ='{loginTextBox.Text}' and haslo = '{hasloTextBox.Text}'";
         }
     }
